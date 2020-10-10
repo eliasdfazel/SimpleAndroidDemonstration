65cf5211e1b4fca007ccd274ba11c9b0ffb9c0b9
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-10-10 10:47:33 -0800

    Initial Commit

---
+++ PhoneApplication/src/main/java/net/geeksempire/simpleandroiddemonstration/EntryPoint.kt
@@ -0,0 +1,20 @@
+package net.geeksempire.simpleandroiddemonstration
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import net.geeksempire.simpleandroiddemonstration.databinding.EntryPointViewBinding
+
+class EntryPoint : AppCompatActivity() {
+
+    private lateinit var entryPointViewBinding: EntryPointViewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        entryPointViewBinding = EntryPointViewBinding.inflate(layoutInflater)
+        setContentView(entryPointViewBinding.root)
+
+
+
+    }
+
+}
