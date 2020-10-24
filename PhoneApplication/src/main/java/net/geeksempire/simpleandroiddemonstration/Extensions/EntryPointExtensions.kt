420a509883079a7f1b581604209dc4957b3f3c74
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-10-24 11:05:11 -0800

    Initial Commit

--- PhoneApplication/src/main/java/net/geeksempire/simpleandroiddemonstration/Extensions/EntryPointExtensions.kt
+++ PhoneApplication/src/main/java/net/geeksempire/simpleandroiddemonstration/Extensions/EntryPointExtensions.kt
@@ -1,10 +1,9 @@
 package net.geeksempire.simpleandroiddemonstration.Extensions
 
-import android.graphics.Color
 import net.geeksempire.simpleandroiddemonstration.EntryPoint
 
 fun EntryPoint.setupColorsOfViews() {
 
-    entryPointViewBinding.newTextView.setTextColor(Color.RED)
+
 
 }
