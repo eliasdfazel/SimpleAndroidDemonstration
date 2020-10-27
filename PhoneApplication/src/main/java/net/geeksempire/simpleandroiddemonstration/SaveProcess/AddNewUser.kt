917c586316f77b160e63d731705df29257ba2a32
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-10-27 12:23:08 -0800

    Initial Commit

--- PhoneApplication/src/main/java/net/geeksempire/simpleandroiddemonstration/SaveProcess/AddNewUser.kt
+++ PhoneApplication/src/main/java/net/geeksempire/simpleandroiddemonstration/SaveProcess/AddNewUser.kt
@@ -2,9 +2,12 @@ package net.geeksempire.simpleandroiddemonstration.SaveProcess
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import net.geeksempire.simpleandroiddemonstration.DatabaseProcess.UserInformationProcess
 import net.geeksempire.simpleandroiddemonstration.databinding.AddNewUserViewBinding
 
 class AddNewUser : AppCompatActivity() {
+
+    val userInformationProcess: UserInformationProcess = UserInformationProcess()
 
     lateinit var addNewUserViewBinding: AddNewUserViewBinding
 
@@ -14,6 +17,19 @@ class AddNewUser : AppCompatActivity() {
         setContentView(addNewUserViewBinding.root)
 
 
+
+        addNewUserViewBinding.saveDataView.setOnClickListener {
+
+            userInformationProcess.saveUserDataInDatabase(
+                context = applicationContext,
+                uniqueUsername = addNewUserViewBinding.usernameView.text.toString(),
+                emailAddress = addNewUserViewBinding.emailAddressView.text.toString(),
+                phoneNumber = addNewUserViewBinding.phoneNumberView.text.toString()
+            )
+
+            this@AddNewUser.finish()
+
+        }
 
     }
 
